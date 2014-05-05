e7a109655c3f32587d45453b1df5c5dfd3048eb4
Jake Lundberg <jlundberg@adconion.com>
2014-05-05 11:16:57 -0700

    git only, need modulepath, testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,23 @@
 #          This will install all resources in a users home directory
 # AUTHORS: Jake Lundberg <code@guru-beach.com>
 
-#Install git tools
-sudo yum install -y git-all
-#Clone the puppet-demo repo
+# Install git tools
+sudo yum install -y git
+# Clone the puppet-demo repo
 if [ ! -d "puppet-demo" ];then
   git clone https://github.com/guru-beach/puppet-demo.git
 fi
 cd puppet-demo
+# Install the yum repo for puppet
 if [ ! -f "/etc/yum.repos.d/puppet-labs.repo" ]; then
   sudo cp puppet-labs.repo /etc/yum.repos.d/
 fi
+# Install the puppet packages.  Hiera isn't being used, but install it anyway
 sudo yum install -y puppet hiera facter
 
 
-#Clone puppet-demo repo
-sudo puppet apply manifests/site.pp
+# Make the magic happen
+sudo puppet apply manifests/site.pp --modulepath=${PWD}/modules
+
+# And the moment we've all been waiting for:
+curl -s http://localhost:8000/
